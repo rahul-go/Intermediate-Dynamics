378771bb3c8c05b46800905d43673600ea0c6ce2
rahul-go <rahulxgoyal@gmail.com>
2018-02-13 14:49:09 -0800

    Begin slip -> no-slip (x, y) framework

--- Lab 4/Rahul_Goyal_main.m
+++ Lab 4/Rahul_Goyal_main.m
@@ -71,7 +71,8 @@
 
 
 %% Hand Calculations
-% The following TODO
+% The hand calculations below provide foundation for the MATLAB solution.
+%
 % <<HandCalculations.jpg>>
 
 
@@ -108,7 +109,14 @@ sim('Integrator');
 
 
 %% Animation
-%  TODO
+%  The following animates the motion of the bowling ball. It plots the lane
+%  and the bowling ball (color dependent on whether the bowling ball is
+%  slipping). Additionally, it stores the frames of the animation
+%  (converted to images) for exporting them as an animated GIF afterwards.
+
+was_slipping = true;            % Initialize slip -> no-slip boolean
+noslip_x0 = -1;                 % Initialize start of no-slip[x]
+noslip_y0 = -1;                 % Initialize start of no-slip[y]
 
 for t = 1:length(tout)
     
@@ -137,6 +145,10 @@ for t = 1:length(tout)
     % If no slip, set bowling ball color to green
     if a == 0
         color = 'g';
+        % If the bowling ball was slipping, TODO
+        if was_slipping
+            ok = 0;
+        end
     % Else, set bowling ball color to blue
     else
         color = 'b';
@@ -174,7 +186,7 @@ end
 
 
 %% Export as GIF
-% The following exports the animation as a GIF.
+% The following exports the animation as an animated GIF.
 
 file_name = 'BowlingBallAnimation.gif';
 for i = 1:length(image)
