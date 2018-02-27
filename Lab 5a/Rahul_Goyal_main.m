48b0b8689b5769ee0122624cfc104eccfc6b9e81
rahul-go <rahulxgoyal@gmail.com>
2018-02-27 03:30:40 -0800

    Commenting; clean up code

--- Lab 5a/Rahul_Goyal_main.m
+++ Lab 5a/Rahul_Goyal_main.m
@@ -14,7 +14,8 @@
 % 
 % *Required Files:*
 %
-% * TODO
+% * Simulator.slx - TODO
+% * link_solver.m - TODO
 %
 % *Still To Do:*
 %
@@ -23,7 +24,9 @@
 
 
 %% Problem Statement
-% TODO
+% Crank OA rotates clockwise at the constant rate theta-dot = 3 rad/s. The
+% connecting link AB passes through the pivoted collar at C. Note: lAB =
+% 350 mm. Develop a complete kinematic model of the slider-crank mechanism.
 
 
 
@@ -40,11 +43,15 @@ clc;
 % The following assigns values given by the problem statement to variables.
 
 % Given Values
-tdot_2 = -3;                    % Angular velocity of TODO (rad/s)
-l_ab = 350/1000;                % TODO (m)
-r_1 = 240/1000;                 % TODO (m)
-r_2 = 80/1000;                  % TODO (m)
-theta2_stop = -4*pi;            % TODO (rad)
+tdot_2 = -3;                    % Angular velocity of link OA (rad/s)
+l_ab = 350/1000;                % Length of link AB (m)
+r_1 = 240/1000;                 % Length of vector R1 (m)
+r_2 = 80/1000;                  % Length of vector R2 (m)
+theta2_stop = -4*pi;            % Theta_2 final (rad)
+
+% Set Values
+t2_0 = deg2rad(0);              % Angular position initial of link OA (rad)
+% t2_0 = deg2rad(140);            % Angular position initial of link OA (rad)
 
 
 
@@ -52,40 +59,40 @@ theta2_stop = -4*pi;            % TODO (rad)
 % The following sets the initial conditions of the slider-crank. See the
 % attached file for hand calculations.
 
-% Set Initial theta_2
-% t2_0 = deg2rad(140);            % Angular position of TODO (rad)
-t2_0 = deg2rad(0);              % Angular position of TODO (rad)
-
 % Position Initial Conditions
+% Length of vector R2 initial (m) [Pythagorean Theorem]
 r3_0 = hypot(r_1+r_2*cos(t2_0), r_2*sin(t2_0));
-                                % TODO (m) [Pythagorean Theorem]
+% Angular position of link OA initial (rad) [Law of Sines]
 t3_0 = asin(sin(pi-t2_0)/r3_0 * r_2);
-                                % Theta_3 Initial (rad) [Law of Sines]
-x2_0 = r_1+r_2*cos(t2_0)/2;     % TODO (m)
-y2_0 = r_2*sin(t2_0)/2;         % TODO (m)
+% COM[x] initial of link OA (m)
+x2_0 = r_1+r_2*cos(t2_0)/2;
+% COM[y] initial of link OA (m)
+y2_0 = r_2*sin(t2_0)/2;
+% COM[x] initial of link AB (m)
 x3_0 = r_1+r_2*cos(t2_0) - l_ab*cos(t3_0)/2;
-                                % TODO (m)
+% COM[y] initial of link AB (m)
 y3_0 = r_2*sin(t2_0) - l_ab*sin(t3_0)/2;
-                                % TODO (m)
 
 % Position Initial Conditions Matrix
-% x_0 = [r3_0, t3_0];
 x_0 = [r3_0, t3_0, x2_0, y2_0, x3_0, y3_0];
 
+
+
 % Velocity Initial Conditions
-rdot3_0 = 0;                    % TODO (m/s) FIX
-tdot3_0 = r_2*tdot_2 / r3_0;    % TODO (rad/s) FIX
+% Velocity of vector R2 initial (m/s) FIX
+rdot3_0 = 0;
+% Angular velocity of link OA initial (rad/s) FIX
+tdot3_0 = r_2*tdot_2 / r3_0;
+% Velocity_G[x] initial of link OA (m/s)
 xdot2_0 = tdot_2*r_2*sin(t2_0) / 2;
-                                % TODO (m/s)
+% Velocity_G[y] initial of link OA (m/s)
 ydot2_0 = tdot_2*r_2*cos(t2_0) / 2;
-                                % TODO (m/s)
+% Velocity_G[x] initial of link AB (m/s)
 xdot3_0 = tdot3_0*r3_0*sin(t3_0) / 2;
-                                % TODO (m/s)
+% Velocity_G[x] initial of link AB (m/s)
 ydot3_0 = tdot3_0*r3_0*cos(t3_0) / 2;
-                                % TODO (m/s)
 
 % Velocity Initial Conditions Matrix
-% v_0 = [rdot3_0, tdot3_0];
 v_0 = [rdot3_0, tdot3_0, xdot2_0, ydot2_0, xdot3_0, ydot3_0];
 
 
@@ -98,8 +105,6 @@ sim('Simulator.slx');
 
 %% Plotting Data Setup
 % TODO
-
-% TODO
 r1_x = [0, r_1];
 r1_y = [0, 0];
 
@@ -108,35 +113,34 @@ r1_y = [0, 0];
 %% Tip-to-Tail Animation
 % TODO
 
-% for t = 1:length(tout)
-% 
-%     % TODO
-%     t_2 = t2_0 + tdot_2 * tout(t);
-% 
-%     % TODO
-%     r2_x = [r1_x(end), r1_x(end) + r_2*cos(t_2)];
-%     r2_y = [r1_y(end), r1_y(end) + r_2*sin(t_2)];
-%     r3_x = [r2_x(end), r1_x(1)];
-%     r3_y = [r2_y(end), r2_y(1)];
-% 
-%     % Plot the links
-%     plot(r1_x, r1_y, r2_x, r2_y, r3_x, r3_y, 'LineWidth', 2);
-%     % Keeps the frame consistent
-%     axis equal;
-%     axis([-0.2, 0.4, -0.1, 0.1]);
-%     
-%     % Calculate the time step and pause accordingly
-%     if t ~= length(tout)            % Prevent index error
-%         % Calculate the time step (s)
-%         t_step = tout(t+1) - tout(t);
-%         pause(t_step);              % Assume negligible processing time
-%     end
-% 
-% end
+for t = 1:length(tout)
+
+    t_2 = t2_0 + tdot_2 * tout(t);  % Angular position of link OA (m)
+
+    % Cartesian Coordinates of Link OA, AB (tip-to-tail)
+    r2_x = [r1_x(end), r1_x(end) + r_2*cos(t_2)];
+    r2_y = [r1_y(end), r1_y(end) + r_2*sin(t_2)];
+    r3_x = [r2_x(end), r1_x(1)];
+    r3_y = [r2_y(end), r2_y(1)];
+
+    % Plot the vector links
+    plot(r1_x, r1_y, r2_x, r2_y, r3_x, r3_y, 'LineWidth', 2);
+    % Keep the frame consistent
+    axis equal;
+    axis([-0.2, 0.4, -0.1, 0.1]);
+    
+    % Calculate the time step and pause accordingly
+    if t ~= length(tout)            % Prevent index error
+        % Calculate the time step (s)
+        t_step = tout(t+1) - tout(t);
+        pause(t_step);              % Assume negligible processing time
+    end
+
+end
 
 
 
-%% Plot of R3 vs. theta_2
+%% Plot of R3 vs. Theta_2
 % TODO
 plot(tdot_2*tout, vout(:, 1));
 
@@ -147,32 +151,31 @@ plot(tdot_2*tout, vout(:, 1));
 
 for t = 1:length(tout)
 
-    % TODO
-    t_2 = t2_0 + tdot_2 * tout(t);
+    t_2 = t2_0 + tdot_2*tout(t);    % Angular position of link OA (m)
     
-    % TODO
-    r_3 = xout(t, 1);               % Easy access to TODO
-    t_3 = xout(t, 2);               % Easy access to TODO
-    x_2 = xout(:, 3);               % Easy access to TODO
-    y_2 = xout(:, 4);               % Easy access to TODO
-    x_3 = xout(:, 5);               % Easy access to TODO
-    y_3 = xout(:, 6);               % Easy access to TODO
+    % Easy Access to...
+    r_3 = xout(t, 1);               % Length of vector R3 (m)
+    t_3 = xout(t, 2);               % Angular position of link AB (rad)
+    x_2 = xout(:, 3);               % COM[x] of link OA (m)
+    y_2 = xout(:, 4);               % COM[y] of link OA (m)
+    x_3 = xout(:, 5);               % COM[x] of link AB (m)
+    y_3 = xout(:, 6);               % COM[y] of link AB (m)
 
-    % TODO
+    % Cartesian Coordinates of Link OA, AB
     r2_x = [r1_x(end), r1_x(end) + r_2*cos(t_2)];
     r2_y = [r1_y(end), r1_y(end) + r_2*sin(t_2)];
     lab_x = [r2_x(end), r2_x(end) - l_ab*cos(t_3)];
     lab_y = [r2_y(end), r2_y(end) - l_ab*sin(t_3)];
 
-    % Plot
+    % Plot the links, COMs, COM paths
     plot(r1_x, r1_y, ...            % Link 1
          r2_x, r2_y, ...            % Link 2
          lab_x, lab_y, ...          % Link AB
-         x_2(1:t), y_2(1:t), ...    % TODO
-         x_3(1:t), y_3(1:t), ...    % TODO
+         x_2(1:t), y_2(1:t), ...    % Path of link OA COM
+         x_3(1:t), y_3(1:t), ...    % Path of link AB COM
          'LineWidth', 2);           % Line Properties
 
-    % Keeps the frame consistent
+    % Keep the frame consistent
     axis equal;
     axis([-0.2, 0.4, -0.1, 0.1]);
     
--- Lab 5a/link_solver.m
+++ Lab 5a/link_solver.m
@@ -7,35 +7,9 @@ function [x] = link_solver(u)
 % The following assigns values given by the problem statement to variables.
 
 % Given Values
-r_1 = 240/1000;                 % TODO (m)
-r_2 = 80/1000;                  % TODO (m)
-l_ab = 350/1000;                % TODO (m)
-
-
-
-%% Easy Access
-% TODO
-% t_2 = u(1);
-% tdot_2 = u(2);
-% r_3 = u(3);
-% t_3 = u(4);
-% rdot_3 = u(5);
-% tdot_3 = u(6);
-
-t_2 = u(1);
-tdot_2 = u(2);
-r_3 = u(3);
-t_3 = u(4);
-x_2 = u(5);
-y_2 = u(6);
-x_3 = u(7);
-y_3 = u(8);
-rdot_3 = u(9);
-tdot_3 = u(10);
-xdot_2 = u(11);
-ydot_2 = u(12);
-xdot_3 = u(13);
-ydot_3 = u(14);
+l_ab = 350/1000;                % Length of link AB (m)
+r_1 = 240/1000;                 % Length of vector R1 (m)
+r_2 = 80/1000;                  % Length of vector R2 (m)
 
 
 
@@ -43,18 +17,20 @@ ydot_3 = u(14);
 % The following assigns values derived and/or solved from the given values
 % to variables. See the attached file for hand calculations.
 
-% A = [cos(t_3), -r_3*sin(t_3);
-%      sin(t_3), r_3*cos(t_3)];
- 
+% Easy Access to...
+t_2 = u(1);                     % Angular position of link OA (rad)
+tdot_2 = u(2);                  % Angular velocity of link OA (rad/s)
+r_3 = u(3);                     % Length of vector R3 (m)
+t_3 = u(4);                     % Angular position of link AB (rad)
+rdot_3 = u(9);                  % Velocity of vector R3 (m/s)
+tdot_3 = u(10);                 % Angular velocity of link AB (rad/s)
+
 A = [cos(t_3), -r_3*sin(t_3), 0, 0, 0, 0;
      sin(t_3), r_3*cos(t_3), 0, 0, 0, 0;
      0, 0, 1, 0, 0, 0;
      0, 0, 0, 1, 0, 0;
      -cos(t_3), (r_3-l_ab/2)*sin(t_3), 0, 0, 1, 0;
      -sin(t_3), -(r_3-l_ab/2)*cos(t_3), 0, 0, 0, 1];
-
-% B = [2*rdot_3*tdot_3*sin(t_3) + r_3*tdot_3^2*cos(t_3) - r_2*tdot_2^2*cos(t_2);
-%      -2*rdot_3*tdot_3*cos(t_3) + r_3*tdot_3^2*sin(t_3) - r_2*tdot_2^2*sin(t_2)];
 
 B = [2*rdot_3*tdot_3*sin(t_3) + r_3*tdot_3^2*cos(t_3) - r_2*tdot_2^2*cos(t_2);
      -2*rdot_3*tdot_3*cos(t_3) + r_3*tdot_3^2*sin(t_3) - r_2*tdot_2^2*sin(t_2);
